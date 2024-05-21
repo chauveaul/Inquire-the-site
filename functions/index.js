d7e412cc1193a0dd929d25e7ef4862f199d67b98
MeZeOfficial <lucaschauveau007@gmail.com>
2024-05-21 09:04:49 -0400

    Made the html for login prompt
    Will add authentication functionality later

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const { onRequest } = require("firebase-functions/v2/https");
 const functions = require("firebase-functions");
 const firebase = require("firebase-admin");
+const { getAuth } = require("firebase-admin/auth");
 const { defineSecret, defineString } = require("firebase-functions/params");
 const logger = require("firebase-functions/logger");
 const engines = require("consolidate");
@@ -78,46 +79,5 @@ exports.ai = onRequest(
     }
   },
 );
-// app.post("/api/ai", async (req, res) => {
-//   console.log("api request");
-//   const userMessage = req.body.userMessage;
-//   try {
-//     if (userMessage == null) {
-//       throw new Error("No prompt was provided.");
-//     }
-
-//     aiResponseKeyword = aiResponseKeyword.split(" ");
-//     console.log(aiResponseKeyword);
-//     console.log(`Keyword: ${aiResponseKeyword}`);
-
-//     const link = await crawler.findURL(
-//       req.body.baseUrl,
-//       req.body.baseUrl,
-//       aiResponseKeyword,
-//       req.body.timer,
-//     );
-
-//     console.log(`Link: ${link}`);
-
-//     let aiResponse = aiResponseKeyword;
-
-//     if (link !== "DNE") {
-//       const htmlTextContent = await scraper.htmlTextContents(link);
-//       aiResponse = await ai.main(userMessage, htmlTextContent);
-//       console.log(`AI Reponse: ${aiResponse}`);
-//     }
-
-//     if (aiResponse === aiResponseKeyword)
-//       aiResponse =
-//         "Sorry, I was not able to find a match. Maybe try again or switch up the url.";
-
-//     return res.status(200).json({
-//       success: true,
-//       message: aiResponse,
-//     });
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// });
 
 exports.app = onRequest(app);
