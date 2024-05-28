f8040cec508e140be3b33890b34e017b118b9258
MeZeOfficial <lucaschauveau007@gmail.com>
2024-05-28 14:03:53 -0400

    Working auth functionality with email/password

--- public/authModule.js
+++ public/authModule.js
@@ -1,5 +1,16 @@
 "use strict";
 
+const signInEmailField = document.querySelector(".email-input-signin");
+const signInPasswordField = document.querySelector(".password-input-signin");
+const signInSubmitButton = document.querySelector(".Sign-in");
+
+const signUpEmailField = document.querySelector(".email-input-signup");
+const signUpPasswordField = document.querySelector(".password-input-signup");
+const signUpPasswordRepeatField = document.querySelector(
+  ".password-input-repeat-signup",
+);
+const signUpSubmitButton = document.querySelector(".sign-up");
+
 firebase.initializeApp({
   apiKey: "AIzaSyAjgCealkLcvoWS7GTJGYNLSgFPSg3F9wI",
   authDomain: "inquire-the-site.firebaseapp.com",
@@ -12,12 +23,55 @@ firebase.initializeApp({
 
 const auth = firebase.auth();
 
-// const googleSignInBtn = document.querySelector(".google-signin");
-// const provider = new firebase.auth.GoogleAuthProvider();
+signInSubmitButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(
+      signInEmailField.value,
+      signInPasswordField.value,
+    )
+    .then((userCredential) => {
+      var user = userCredential.user;
+      console.log(`Logged in as: ${user}`);
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(`Error Code: ${errorCode}\nError Message: ${errorMessage}`);
+    });
+});
 
-// googleSignInBtn.addEventListener("click", function () {
-//   auth.signInWithPopup(provider);
-// });
+signUpSubmitButton.addEventListener("click", function (e) {
+  console.log(
+    `email: ${signUpEmailField.value} \n password: ${signUpPasswordField.value}`,
+  );
+  e.preventDefault();
+  if (signUpPasswordField.value !== signUpPasswordRepeatField.value) {
+    console.warn("Passwords must be identical");
+  } else {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(
+        signUpEmailField.value,
+        signUpPasswordField.value,
+      )
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        console.log(`Logged in as ${user}`);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(
+          `Error Code: ${errorCode} \nError Message: ${errorMessage}`,
+        );
+        // ..
+      });
+  }
+});
 
 auth.onAuthStateChanged(auth, (user) => {
   if (user) {
